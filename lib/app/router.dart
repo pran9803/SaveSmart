40618a60b9c83970e36e74483a1549973b57fd9a
Pranaya Jayaprakash <pranayawarrier07@gmail.com>
2026-09-23 11:34:15 +0530

    Add authentication screen and login route

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,11 +1,16 @@
 import 'package:go_router/go_router.dart';
 
+import '../screens/auth/login_screen.dart';
 import '../screens/home/home_screen.dart';
 import 'routes.dart';
 
 final appRouter = GoRouter(
-  initialLocation: AppRoutes.home,
+  initialLocation: AppRoutes.login,
   routes: [
+    GoRoute(
+      path: AppRoutes.login,
+      builder: (context, state) => const LoginScreen(),
+    ),
     GoRoute(
       path: AppRoutes.home,
       builder: (context, state) => const HomeScreen(),
